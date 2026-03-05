d2def780fd8d34dcd263534db830cdabe137c774
Rich Renomeron <rrenomeron+github@gmail.com>
2026-03-04 19:24:29 -0500

    fix: allow flatpak preinstall to work

--- build/49-unsorted-packages.sh
+++ build/49-unsorted-packages.sh
@@ -9,3 +9,12 @@ dnf5 -y install \
     gnome-shell-extension-dash-to-dock \
     gnome-shell-extension-blur-my-shell \
     gnome-tweaks
+
+# This adds the version of flatpak that can do preinstalls
+# Remove when Fedora 44 is a thing
+  dnf -y copr enable ublue-os/flatpak-test
+  dnf -y copr disable ublue-os/flatpak-test
+  dnf -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak flatpak
+  dnf -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-libs flatpak-libs
+  dnf -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-session-helper flatpak-session-helper
+  dnf -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test install flatpak-debuginfo flatpak-libs-debuginfo flatpak-session-helper-debuginfo
